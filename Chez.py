503bc4b6607b697fb3b22f5a2939cae759b00934
Alex-Hamish <125255039+Alex-Hamish@users.noreply.github.com>
2025-02-02 12:59:21 +1100

    Made Chez.py Chez-lower.py

--- Chez.py
+++ Chez.py
@@ -1,32 +1,106 @@
+import re
+
+#Here The Functions Lie
+def FindBracketedText(text):
+    x = re.search("(?<=\().+?(?=\))", text)
+    return x.group()
+def FindBracketedString(text):
+    x = re.search("(?<=\(\").+?(?=\"\))", text)
+    return x
+def hasechogood(text):
+    x = re.findall("echo\(\"[^\"]*\"\)", text)
+    if (x):
+        return True
+    else:
+        return False
+def hasechobad(text):
+    x = re.findall("echo\([^\)]*\)", text)
+    if (x):
+        return True
+    else:
+        return False
+    
 print("Welcome To Chez Console")
 while True:
-    CP = input("")
-    if CP == "Help":
+    Input = input("")
+    CurrentCommand = Input.lower()
+    if CurrentCommand == "help":
         print("Do Help() For All Commands")
-    elif CP == "Help()":
+    elif CurrentCommand == "help()":
         print("""Help() Shows All Commands
 End() Ends The Program
 Error() Shows What Errors Are
 Echo() Responds With The Input""")
-    elif CP == "End()":
+    elif CurrentCommand == "end()":
         print("Bye Bye!!")
         break
-    elif CP == "Error()":
+    elif CurrentCommand == "error()":
         print("Put The Command With The First Letters To Learn About It")
-    elif CP == "Error(NCE)":
+    elif CurrentCommand == "error(nce)":
         print("NCE Means There Is No Current Command For That")
-    elif CP == "Error(NIE)":
+    elif CurrentCommand == "error(nie)":
         print("NIE Means There Is A Blank Space In An Input")
-    elif CP == "Echo()":
-        Echo = input("""What Would You Like To Echo?
-""")
-        if Echo == "":
-            print("NIE")
-        else:
-            print(Echo)
-    elif CP == "":
+    elif CurrentCommand == "error(se)":
+        print("SE Means There Is A Syntax Error")
+    elif CurrentCommand == "echo()":
         print("NIE")
+    elif hasechobad(CurrentCommand):
+        if hasechogood(CurrentCommand):
+            print(FindBracketedString(Input).group())
+        else:   
+            print("SE")
     else:
         print("NCE")
-        
+import re
 
+#Here The Functions Lie
+def FindBracketedText(text):
+    x = re.search("(?<=\().+?(?=\))", text)
+    return x.group()
+def FindBracketedString(text):
+    x = re.search("(?<=\(\").+?(?=\"\))", text)
+    return x
+def hasechogood(text):
+    x = re.findall("echo\(\"[^\"]*\"\)", text)
+    if (x):
+        return True
+    else:
+        return False
+def hasechobad(text):
+    x = re.findall("echo\([^\)]*\)", text)
+    if (x):
+        return True
+    else:
+        return False
+    
+print("Welcome To Chez Console")
+while True:
+    Input = input("")
+    CurrentCommand = Input.lower()
+    if CurrentCommand == "help":
+        print("Do Help() For All Commands")
+    elif CurrentCommand == "help()":
+        print("""Help() Shows All Commands
+End() Ends The Program
+Error() Shows What Errors Are
+Echo() Responds With The Input""")
+    elif CurrentCommand == "end()":
+        print("Bye Bye!!")
+        break
+    elif CurrentCommand == "error()":
+        print("Put The Command With The First Letters To Learn About It")
+    elif CurrentCommand == "error(nce)":
+        print("NCE Means There Is No Current Command For That")
+    elif CurrentCommand == "error(nie)":
+        print("NIE Means There Is A Blank Space In An Input")
+    elif CurrentCommand == "error(se)":
+        print("SE Means There Is A Syntax Error")
+    elif CurrentCommand == "echo()":
+        print("NIE")
+    elif hasechobad(CurrentCommand):
+        if hasechogood(CurrentCommand):
+            print(FindBracketedString(Input).group())
+        else:   
+            print("SE")
+    else:
+        print("NCE")
