e1cde21440aa683c41c23d4fed588f44b65e5387
Alex-Hamish <125255039+Alex-Hamish@users.noreply.github.com>
2025-02-02 12:57:21 +1100

    Added Echo Command

--- Chez-lower.py
+++ Chez-lower.py
@@ -1,27 +1,53 @@
+import re
+
+#Here The Functions Lie
+def FindBracketedText(text):
+    x = re.search("(?<=\().+?(?=\))", text)
+    return x.group()
+def FindBracketedString(text):
+    x = re.search("(?<=\(\").+?(?=\"\))", text)
+    return x
+def hasechogood(text):
+    x = re.findall("echo\(\"[^\"]*\"\)", text)
+    if (x):
+        return True
+    else:
+        return False
+def hasechobad(text):
+    x = re.findall("echo\([^\)]*\)", text)
+    if (x):
+        return True
+    else:
+        return False
+    
 print("Welcome To Chez Console")
-# Put Functions Here!
 while True:
     Input = input("")
-    CP = Input.lower()
-    if CP == "help":
+    CurrentCommand = Input.lower()
+    if CurrentCommand == "help":
         print("Do Help() For All Commands")
-    elif CP == "help()":
+    elif CurrentCommand == "help()":
         print("""Help() Shows All Commands
 End() Ends The Program
 Error() Shows What Errors Are
 Echo() Responds With The Input""")
-    elif CP == "end()":
+    elif CurrentCommand == "end()":
         print("Bye Bye!!")
         break
-    elif CP == "error()":
+    elif CurrentCommand == "error()":
         print("Put The Command With The First Letters To Learn About It")
-    elif CP == "error(nce)":
+    elif CurrentCommand == "error(nce)":
         print("NCE Means There Is No Current Command For That")
-    elif CP == "error(nie)":
+    elif CurrentCommand == "error(nie)":
         print("NIE Means There Is A Blank Space In An Input")
-    elif CP == "echo()":
-        print("ECHO IS BEING TESTED")
+    elif CurrentCommand == "error(se)":
+        print("SE Means There Is A Syntax Error")
+    elif CurrentCommand == "echo()":
+        print("NIE")
+    elif hasechobad(CurrentCommand):
+        if hasechogood(CurrentCommand):
+            print(FindBracketedString(Input).group())
+        else:   
+            print("SE")
     else:
         print("NCE")
-         
-
